eee41d0fa9084b76dfd0c6dd4174d691d5ca7564
barkerrw <barkerrw@rose-hulman.edu>
2023-03-15 16:36:40 -0400

    second commit

--- india-engd250/src/MainApp.java
+++ india-engd250/src/MainApp.java
@@ -4,7 +4,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
+		System.out.println("Yo!");
 	}
 
 }
