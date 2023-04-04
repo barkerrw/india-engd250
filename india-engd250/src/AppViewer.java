c75ad8ab77a7445db091fd35acd6a0cfcf164d65
rhit-altschmn <altschmn@rose-hulman.edu>
2023-04-04 10:55:38 -0400

    map 2 with bad buttons

--- india-engd250/src/AppViewer.java
+++ india-engd250/src/AppViewer.java
@@ -16,48 +16,69 @@ public class AppViewer {
 	public static final int HEIGHT = 600;
 	private ImageIcon map;
 	
+	int mapX;
+	int mapY;
+	
 	public AppViewer() {
 		JFrame frame = new JFrame("Start Screen");
 		frame.setSize(WIDTH, HEIGHT);
+//		frame.setLayout(null);
 		
-//		JPanel panel = new JPanel();
-		JLabel label = new JLabel("Where do you want to go?");
-//		JButton rural = new JButton("Rural");
-//		JButton urban = new JButton("Urban");
-		
-//		Dimension buttonSize = new Dimension(150,200);
-//	
-//		label.setHorizontalAlignment(JLabel.CENTER);
-//		
+		JPanel panel = new JPanel();
+		Dimension buttonSize = new Dimension(50,100);
 //		rural.setPreferredSize(buttonSize);
 //		urban.setPreferredSize(buttonSize);
-//		
 //		panel.add(label, BorderLayout.NORTH);
-//		
-//		panel.add(rural);
-//		panel.add(urban);
+		
+		
+		JLabel label = new JLabel("Where do you want to go?");
+		label.setHorizontalAlignment(JLabel.CENTER);
 		
 		map = new ImageIcon("C:/Users/altschmn/git/india-engd250/india-engd250/src/pictures/RegionMap.jpg");
 		
 		JLabel mapHolder = new JLabel(map);
-//		mapHolder.setIcon(map);
-		
-		frame.add(mapHolder);
 		
 		
+		frame.add(mapHolder, BorderLayout.CENTER);
+		frame.add(label, BorderLayout.NORTH);
+		
+				
 		
 		
+//		JButton north = new JButton("North");
+//		JButton west = new JButton("West");
+//		JButton south = new JButton("South");
+//		JButton east = new JButton("East");
+//		JButton northeast = new JButton("NorthEast");
+//		JButton central = new JButton("Central");
+//		
+//		mapX = label.getX();
+//		mapY = label.getY();
+//
+//		
+//		north.setBounds(mapX, mapY, 50,100);
+//		west.setBounds(mapX + 20, mapY + 20, 50,100);
+//		east.setBounds(mapX + 50, mapY + 50, 50,100);
+//		south.setBounds(mapX + 75, mapY + 75, 50,100);
+//		northeast.setBounds(mapX + 100, mapY + 100, 50,100);
+//		central.setBounds(mapX + 125, mapY + 125, 50,100);
+//		
+//		panel.add(north);
+//		panel.add(south);
+//		panel.add(east);
+//		panel.add(west);
+//		panel.add(northeast);
+//		panel.add(central);
 		
 		
+//		panel.repaint();
+	
+	
 		
+//		frame.add(panel);
 		
-		frame.add(mapHolder, BorderLayout.SOUTH);
+		frame.repaint();
 		
-		frame.add(label, BorderLayout.CENTER);
-		
-		
-		
-
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
