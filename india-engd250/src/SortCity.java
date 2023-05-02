5360cb521c092fca523ceea9020ed1c9aebed5be
rhit-altschmn <altschmn@rose-hulman.edu>
2023-05-02 09:01:59 -0400

    beginning sort

---
+++ india-engd250/src/SortCity.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class SortCity {
+	
+	private static ArrayList<City> north;
+	private static ArrayList<City> south;
+	private static ArrayList<City> east;
+	private static ArrayList<City> west;
+	private static ArrayList<City> central;
+	private static ArrayList<City> northeast;
+	
+	
+	
+	public SortCity(ArrayList<City> northCities, ArrayList<City> southCities, ArrayList<City> eastCities, ArrayList<City> westCities, ArrayList<City> centralCities, ArrayList<City> northeastCities) {
+		this.north = northCities;
+		this.south = southCities;
+		this.east = eastCities;
+		this.west = westCities;
+		this.central = centralCities;
+		this.northeast = northeastCities;
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
