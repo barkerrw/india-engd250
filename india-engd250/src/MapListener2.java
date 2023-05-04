2c82ec97c50eb66314d8a51e8891aeee0553d389
rhit-altschmn <altschmn@rose-hulman.edu>
2023-05-04 09:34:44 -0400

    more sorting things using arraylist

--- india-engd250/src/MapListener2.java
+++ india-engd250/src/MapListener2.java
@@ -10,24 +10,30 @@ import java.util.HashMap;
  *
  */
 public class MapListener2 implements ActionListener {
-//	private ArrayList<City> cities = new ArrayList<City>();
-	private HashMap<City,String> allCities = new HashMap<City,String>();
+	private ArrayList<City> allCities = new ArrayList<City>();
+	private int[] regionSize = new int[6];
+	
+//	private HashMap<City,String> allCities = new HashMap<City,String>();
+	
 	
 	/**
 	 * ensures: new MapListener is created
 	 * 
 	 * @param cities the region's cities
 	 */
-	public MapListener2(HashMap<City,String> cities) {
+	public MapListener2(ArrayList<City> cities, int[] sizes) {
 		this.allCities = cities;
-	} // MapListener
+		this.regionSize = sizes;
+	} 
+	
+	// MapListener
 	
 	/**
 	 * ensures: the user input makes a new Page
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		SortCityViewer allCitiesPage = new SortCityViewer(this.allCities);
+		SortCityViewer allCitiesPage = new SortCityViewer(this.allCities, regionSize);
 	} // actionPerformed
 
 } // end MapListener
--- india-engd250/src/SortCity.java
+++ india-engd250/src/SortCity.java
@@ -1,37 +1,59 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 
 public class SortCity {
 	
-	private static HashMap<City,String> allCitiesMap;
+//	private static HashMap<City,String> allCitiesMap;
+//	
+//	public SortCity(HashMap<City,String> cities) {
+//		this.allCitiesMap = cities;
+//	}
+//	
 	
-	public SortCity(HashMap<City,String> cities) {
-		this.allCitiesMap = cities;
-	}
-	
-	
-	private static ArrayList<City> north;
-	private static ArrayList<City> south;
-	private static ArrayList<City> east;
-	private static ArrayList<City> west;
-	private static ArrayList<City> central;
-	private static ArrayList<City> northeast;
-	private static ArrayList<City> allCities;
+	private ArrayList<City> north = new ArrayList<City>();
+	private ArrayList<City> south = new ArrayList<City>();
+	private ArrayList<City> east = new ArrayList<City>();
+	private ArrayList<City> west = new ArrayList<City>();
+	private ArrayList<City> central = new ArrayList<City>();
+	private ArrayList<City> northeast = new ArrayList<City>();
+	private ArrayList<City> allCities;
 	
 	
 	public SortCity(ArrayList<City> fullCityList, int[] regionSize) {
 		
 		this.allCities = fullCityList;
 		
+		for (int i = 0; i < allCities.size(); i ++) {
+			if(i < regionSize[0]) {
+				this.north.add(allCities.get(i));
+			}
+			else if (i >= regionSize[0] && i < regionSize[1]){
+				this.south.add(allCities.get(i));
+			}
+			else if (i >= regionSize[1] && i < regionSize[2]){
+				this.east.add(allCities.get(i));
+			}
+			else if (i >= regionSize[2] && i < regionSize[3]){
+				this.west.add(allCities.get(i));
+			}
+			else if (i >= regionSize[3] && i < regionSize[4]){
+				this.central.add(allCities.get(i));
+			}
+			else {
+				this.northeast.add(allCities.get(i));
+			}
+		}
 		
-		this.north = (ArrayList<City>) fullCityList.subList(0, regionSize[0]);
-		this.south = (ArrayList<City>) fullCityList.subList(regionSize[0], regionSize[1]);
-		this.east = (ArrayList<City>) fullCityList.subList(regionSize[1], regionSize[2]);
-		this.west = (ArrayList<City>) fullCityList.subList(regionSize[2], regionSize[3]);
-		this.central = (ArrayList<City>) fullCityList.subList(regionSize[3], regionSize[4]);
-		this.northeast = (ArrayList<City>) fullCityList.subList(regionSize[4], regionSize[5]);
+		
+//		this.north =  fullCityList.subList(0, regionSize[0]);
+//		this.south =  fullCityList.subList(regionSize[0], regionSize[1]);
+//		this.east = fullCityList.subList(regionSize[1], regionSize[2]);
+//		this.west = (ArrayList<City>) fullCityList.subList(regionSize[2], regionSize[3]);
+//		this.central = (ArrayList<City>) fullCityList.subList(regionSize[3], regionSize[4]);
+//		this.northeast = (ArrayList<City>) fullCityList.subList(regionSize[4], regionSize[5]);
 		
 //		this.allCities.addAll(northCities);
 //		this.allCities.addAll(southCities);
@@ -88,12 +110,43 @@ public class SortCity {
 	}
 	
 	
-//	public ArrayList<City> sortAll(ArrayList<City> cities){
-//		ArrayList<City> sortedCities = cities;
-//		sortedCities.sort(new alphabetical());
-//
-//		return sortedCities;
-//	}
+	public ArrayList<City> sortList(ArrayList<City> cities){
+		ArrayList<City> sortedCities = cities;
+		sortedCities.sort(new alphabetical());
+
+		return sortedCities;
+	}
+	
+	public ArrayList<City> getNorth(){
+		return this.north;
+	}
+	
+	public ArrayList<City> getSouth(){
+		return this.south;
+	}
+	
+	public ArrayList<City> getEast(){
+		return this.east;
+	}
+	
+	public ArrayList<City> getWest(){
+		return this.west;
+	}
+	
+	public ArrayList<City> getCentral(){
+		return this.central;
+	}
+	
+	public ArrayList<City> getNortheast(){
+		return this.northeast;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
